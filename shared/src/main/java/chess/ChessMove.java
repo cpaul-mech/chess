31e88f9f26a0d95c5afeb77eb3591a22cd462a36
Christopher Paul <124709632+cpaul-mech@users.noreply.github.com>
2024-09-17 22:06:01 -0600

    Added Overrides for hashcode and equals

--- shared/src/main/java/chess/ChessMove.java
+++ shared/src/main/java/chess/ChessMove.java
@@ -28,6 +28,21 @@ public class ChessMove {
 
         return "move: "+startPos+" "+endPos;
     }
+    @Override
+    public boolean equals(Object obj) {
+        if(this == obj) return true; //checks for if the memory addresses are the same.
+        if(obj == null || getClass() != obj.getClass()) return false; //checks for if the object's class is equal to the current class
+        ChessMove move = (ChessMove)obj; //sets a new variable move equal to the object and casts object to a ChessMove Object.
+        return (_startPosition.equals(move._startPosition) && _endPosition.equals(move._endPosition) &&
+                _promotionPiece == move._promotionPiece);
+    }
+    @Override
+    public int hashCode() {
+        int promotionCode;
+        if (_promotionPiece == null) promotionCode = 9;
+        else promotionCode = _promotionPiece.ordinal();
+        return (71*_startPosition.hashCode()) + _endPosition.hashCode() + promotionCode;
+    }
     /**
      * @return ChessPosition of starting location
      */
