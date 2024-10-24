fa7fb3a8b2d97f35188745eb01eaa7707c587674
Christopher Paul <124709632+cpaul-mech@users.noreply.github.com>
2024-10-23 23:55:25 -0600

    almost finished

--- server/src/test/java/service/GameServiceTests.java
+++ server/src/test/java/service/GameServiceTests.java
@@ -1,9 +1,10 @@
 package service;
 
+import dataaccess.DataAccessException;
+import model.GameData;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class GameServiceTests {
     //initialize DAO's
@@ -15,13 +16,9 @@ public class GameServiceTests {
         assertEquals(GAME_SERVICE.sizeof(), 0);
     }
 
-    @Test
-    void getGame() {
-//
-    }
 
     @Test
-    void createGame() {
+    void goodcreateGame() {
         int gameID = GAME_SERVICE.createGame("myGame");
         assertNotEquals(0, gameID);
     }
@@ -34,8 +31,29 @@ public class GameServiceTests {
     }
 
     @Test
-    void goodUpdateGame() {
+    void goodUpdateGame() throws DataAccessException {
+        int gameID3 = GAME_SERVICE.createGame("oof");
+        GAME_SERVICE.updateGame("white", gameID3, "thisGuy");
+        assertEquals("thisGuy", GAME_SERVICE.getGame(gameID3).whiteUsername());
+    }
 
+    @Test
+    void badUpdateGame() {
+        assertThrows(BadServiceRequest.class, () ->
+                GAME_SERVICE.updateGame(null, 2, "thisGuy"));
+    }
+
+    @Test
+    void goodGetGame() {
+        int gameID4 = GAME_SERVICE.createGame("oops");
+        var result = GAME_SERVICE.getGame(gameID4);
+        assertEquals(GameData.class, result.getClass());
+    }
+
+    @Test
+    void badGetGame() {
+        var result = GAME_SERVICE.getGame(0);
+        assertNull(result);
     }
 
 
