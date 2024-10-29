e4963a606f07f29fe2ad378e226212c6965f840c
Christopher Paul <124709632+cpaul-mech@users.noreply.github.com>
2024-10-28 22:31:50 -0600

    continued development of testing framework

--- server/src/test/java/dataaccess/SQLGameDAOTests.java
+++ server/src/test/java/dataaccess/SQLGameDAOTests.java
@@ -1,39 +1,11 @@
 package dataaccess;
 
-import model.GameData;
+import org.junit.jupiter.api.Test;
 
-import java.util.Collection;
-import java.util.List;
-
-public class SQLGameDAOTests implements GameDataAccess {
-    //TODO: IMPLEMENT THESE METHODS!!
-    @Override
-    public int createGame(String gameName) {
-        return 0;
-    }
-
-    @Override
-    public GameData getGame(int gameID) {
-        return null;
-    }
-
-    @Override
-    public Collection<GameData> listGames() {
-        return List.of();
-    }
-
-    @Override
-    public void updateGame(int gameIDtoChange, GameData replacementGame) {
+public class SQLGameDAOTests {
+    @Test
+    public void configureDB() {
 
     }
 
-    @Override
-    public void clearDB() {
-
-    }
-
-    @Override
-    public int dbSize() {
-        return 0;
-    }
 }
