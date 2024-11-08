1ef366b9a99d908571203d9bd59a0f8f95fa8774
Christopher Paul <124709632+cpaul-mech@users.noreply.github.com>
2024-11-07 22:29:24 -0700

    implemented parameter isolation from input commands

--- client/src/main/java/ui/LoggedOutClient.java
+++ client/src/main/java/ui/LoggedOutClient.java
@@ -1,11 +1,13 @@
 package ui;
 
+import java.util.Arrays;
+
 public class LoggedOutClient {
     public LoggedOutClient() {
 
     }
 
-    public String eval(String line) {
+    public String eval(String line) throws BadInputException {
         //need to parse the line
         var tokens = line.toLowerCase().split(" ");
         if (tokens.length == 0) {
@@ -13,12 +15,23 @@ public class LoggedOutClient {
         } else {
             //cases are help, quit, login, register
             String cmd = tokens[0];
+            String[] params = null;
+            if (tokens.length > 1) {
+                params = Arrays.copyOfRange(tokens, 1, tokens.length);
+            }
             return switch (cmd) {
                 case "help" -> loggedOutHelp();
                 case "quit" -> "quit";
-                default -> "input: " + line + " was not understood. \nProper commands are:\n" + loggedOutHelp();
+                case "login" -> login(params);
+                default -> "cmd: " + cmd + " was not understood. \nProper commands are:\n" + loggedOutHelp();
             };
         }
+    }
+
+    public String login(String[] params) throws BadInputException {
+        if (params == null || params.length < 3) {
+            throw new BadInputException("cmd: login did not have enough parameters.");
+        } //TODO: IMPLEMENT HTTP CODE.
     }
 
     public String loggedOutHelp() {
