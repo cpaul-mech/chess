90d2931b81b4e8f3c878ecc84c1258b71c20fbd0
Christopher Paul <124709632+cpaul-mech@users.noreply.github.com>
2024-11-14 23:23:18 -0700

    changed the file tree

---
+++ client/src/test/java/client/ReplTests.java
@@ -0,0 +1,30 @@
+package client;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import server.Server;
+import ui.EscapeSequences;
+import ui.OverallRepl;
+import ui.ServerFacade;
+
+public class ReplTests {
+    private static Server server;
+    private static OverallRepl overallRepl;
+    private static String serverUrl;
+
+    @BeforeAll
+    public static void init() {
+        server = new Server();
+        var port = server.run(0);
+        System.out.println("Started test HTTP server on " + port);
+        serverUrl = "http://localhost:" + port;
+        overallRepl = new OverallRepl(serverUrl);
+    }
+
+    @Test
+    public void runTest() {
+        overallRepl.run();
+    }
+
+
+}
